106898b1c04dafd0295167c95a18a9396f0b27dd
Denis Cherkasskiy <cherkasskiydenis@yandex.ru>
2024-05-10 12:58:15 +0300

    fix: название функции, alt

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,26 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createCard(cardItem, cardDelete) {
+function createCard(cardItem, deleteCard) {
     const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = placesItem.querySelector('.card__image');
     const cardTitle = placesItem.querySelector('.card__title');
     const cardDeleteButton = placesItem.querySelector('.card__delete-button');
 
     cardImage.src = cardItem.link;
+    cardImage.alt = cardItem.alt;
     cardTitle.textContent = cardItem.name;
-    cardDeleteButton.addEventListener('click', cardDelete);
+    cardDeleteButton.addEventListener('click', deleteCard);
 
     return placesItem;
 }
 // @todo: Функция удаления карточки
-function cardDelete(event) {
+function deleteCard(event) {
     const listItem = event.target.closest('.places__item');
     listItem.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(cardItem){
-    const card = createCard(cardItem, cardDelete);
+    const card = createCard(cardItem, deleteCard);
     placesList.append(card);
 })
